f195e5dd97b4f285593c38dc8f9ee6baf9cd5cd4
pxckets <49133592+pxckets@users.noreply.github.com>
2019-09-26 15:42:02 -0400

    comments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtlecoin import TurtleCoind
 import json
 
+# Request node IP from user
 while True:
     try:
         rpc_host = input('Daemon IP: ')
@@ -11,14 +12,19 @@ while True:
 rpc_port = 11246
 oscillated = TurtleCoind(rpc_host, rpc_port)
 
+#blockexplorer
 restart = "y"
 while restart == "y":
+
+    # request blockheight from user
     custom_height = input("Enter Block Height: ")
     custom_header = oscillated.get_block_header_by_height(int(custom_height))
 
+    # load our Json
     custom_header_dump = json.dumps(custom_header)
     custom_header_loads = json.loads(custom_header_dump)
  
+    # use the data from the JSON
     custom_block_height = custom_height
     custom_block_size = custom_header_loads['result']['block_header']['block_size']
     custom_block_hash = custom_header_loads['result']['block_header']['hash']
@@ -26,6 +32,7 @@ while restart == "y":
     custom_block_tx_count = custom_header_loads['result']['block_header']['num_txes']
     custom_block_hashrate = round(custom_header_loads['result']['block_header']['difficulty'] / 60 / 1000, 2)
 
+    # GUI
     print("")
     print("                                Block " + str(custom_block_height) + " Info")
     print("")
@@ -42,4 +49,5 @@ while restart == "y":
     print("[-------------------------------------------------------------------------------]")
     print("")
 
+    # yeet or skeet
     restart = input("Do you want to check another block? y/n: ")
